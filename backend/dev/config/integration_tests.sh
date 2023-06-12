15e8b8dfedaf7bfc77ce3f18c87f0a3ad69ff187
Krzysztof Bogacki <krzysztof.bogacki@leancode.pl>
2023-06-12 15:45:54 +0200

    Remove unnecessary config keys

--- backend/dev/config/integration_tests.sh
+++ backend/dev/config/integration_tests.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-export Domains__Public=localhost
-export Domains__ApiInternal=localhost
-
 #TODO: Fix once corelib is fixed
 export SqlServer__ConnectionStringBase='Server=mssql-svc.shared.svc.cluster.local,1433;User Id=sa;Password=Passw12#;Encrypt=false'
 export BlobStorage__ConnectionString='DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://storage.local.lncd.pl:10000/devstoreaccount1;'
