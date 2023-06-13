4ac0be020687fb6244b142877656b5cb0d358bbf
Krzysztof Bogacki <krzysztof.bogacki@leancode.pl>
2023-06-13 17:40:07 +0200

    Add migration for Employees and Projects

--- backend/src/Apps/ExampleApp.Migrations/Migrations/20230613153744_AddEmployeesAndProjects.Designer.cs
+++ backend/src/Apps/ExampleApp.Migrations/Migrations/20230613153744_AddEmployeesAndProjects.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ExampleApp.Core.Services.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ExampleApp.Migrations.Migrations
 {
     [DbContext(typeof(CoreDbContext))]
-    partial class CoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230613153744_AddEmployeesAndProjects")]
+    partial class AddEmployeesAndProjects
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
