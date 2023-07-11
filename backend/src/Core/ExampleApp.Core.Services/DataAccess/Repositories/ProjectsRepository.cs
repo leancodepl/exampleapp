83da0852f885dd228ddc60abc22cfca33c7c4fdc
Krzysztof Bogacki <krzysztof.bogacki@leancode.pl>
2023-07-11 19:44:19 +0200

    Workaround EFRepository assigning non-UTC timestamps

--- backend/src/Core/ExampleApp.Core.Services/DataAccess/Repositories/ProjectsRepository.cs
+++ backend/src/Core/ExampleApp.Core.Services/DataAccess/Repositories/ProjectsRepository.cs
@@ -1,5 +1,7 @@
 using ExampleApp.Core.Domain.Projects;
 using LeanCode.DomainModels.EF;
+using LeanCode.DomainModels.Model;
+using LeanCode.TimeProvider;
 using Microsoft.EntityFrameworkCore;
 
 namespace ExampleApp.Core.Services.DataAccess.Repositories;
@@ -8,6 +10,32 @@ public class ProjectsRepository : EFRepository<Project, ProjectId, CoreDbContext
 {
     public ProjectsRepository(CoreDbContext dbContext)
         : base(dbContext) { }
+
+    public override void Add(Project entity)
+    {
+        ((IOptimisticConcurrency)entity).DateModified = Time.NowWithOffset.UtcDateTime;
+        DbSet.Add(entity);
+    }
+
+    public override void Delete(Project entity)
+    {
+        ((IOptimisticConcurrency)entity).DateModified = Time.NowWithOffset.UtcDateTime;
+        DbSet.Remove(entity);
+    }
+
+    public override void DeleteRange(IEnumerable<Project> entities)
+    {
+        foreach (var item in entities)
+        {
+            ((IOptimisticConcurrency)item).DateModified = Time.NowWithOffset.UtcDateTime;
+        }
+        DbSet.RemoveRange(entities);
+    }
+
+    public override void Update(Project entity)
+    {
+        ((IOptimisticConcurrency)entity).DateModified = Time.NowWithOffset.UtcDateTime;
+    }
 
     public override Task<Project?> FindAsync(ProjectId id, CancellationToken cancellationToken = default)
     {
