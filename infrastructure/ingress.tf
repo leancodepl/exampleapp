189911a050a193f17f1ac11622a09a242e590c25
Krzysztof Bogacki <krzysztof.bogacki@leancode.pl>
2023-07-25 16:39:49 +0200

    Add basic ingress routes configuration to infrastructure

---
+++ infrastructure/ingress.tf
@@ -0,0 +1,116 @@
+locals {
+  ingress = {
+    strip = ["/api"]
+    hosts = ["api.${var.domain}", var.domain]
+    rules = [
+      {
+        rule     = "Host(`api.${var.domain}`)"
+        service  = "${local.project}-api"
+        port     = 80
+        priority = 1
+      },
+      {
+        rule     = "Host(`${var.domain}`) && PathPrefix(`/api/`)"
+        service  = "${local.project}-api"
+        port     = 80
+        priority = 1
+      },
+    ]
+  }
+}
+
+resource "kubernetes_manifest" "path_stripping" {
+  manifest = {
+    "apiVersion" = "traefik.containo.us/v1alpha1"
+    "kind"       = "Middleware"
+    "metadata" = {
+      "name"      = "${local.project}-strip-prefix"
+      "namespace" = data.kubernetes_namespace_v1.main.metadata[0].name
+      "labels"    = local.tags
+    }
+    "spec" = {
+      "stripPrefix" = {
+        "prefixes" = local.ingress.strip
+      }
+    }
+  }
+}
+
+resource "kubernetes_manifest" "response_compression" {
+  manifest = {
+    "apiVersion" = "traefik.containo.us/v1alpha1"
+    "kind"       = "Middleware"
+    "metadata" = {
+      "name"      = "${local.project}-compress-response"
+      "namespace" = data.kubernetes_namespace_v1.main.metadata[0].name
+      "labels"    = local.tags
+    }
+    "spec" = {
+      "compress" = {}
+    }
+  }
+}
+
+resource "kubernetes_manifest" "ingress" {
+  manifest = {
+    "apiVersion" = "traefik.containo.us/v1alpha1"
+    "kind"       = "IngressRoute"
+    "metadata" = {
+      "name"      = "${local.project}-ingress"
+      "namespace" = data.kubernetes_namespace_v1.main.metadata[0].name
+      "labels"    = local.tags
+    }
+    "spec" = {
+      "entryPoints" = [
+        "web",
+        "websecure",
+      ]
+      "routes" = [
+        for i in local.ingress.rules : {
+          "kind"  = "Rule"
+          "match" = i.rule
+          "middlewares" = [
+            {
+              "name" = kubernetes_manifest.path_stripping.manifest.metadata.name
+            },
+            {
+              "name" = kubernetes_manifest.response_compression.manifest.metadata.name
+            },
+          ]
+          "services" = [
+            {
+              "name" = i.service
+              "port" = i.port
+            },
+          ]
+          "priority" = i.priority
+        }
+      ]
+
+      "tls" = {
+        "certResolver" = "le"
+      }
+    }
+  }
+}
+
+// https://github.com/traefik/traefik/issues/4655#issuecomment-878072714
+resource "kubernetes_manifest" "ingress_external_dns_hack" {
+  manifest = {
+    "apiVersion" = "networking.k8s.io/v1"
+    "kind"       = "Ingress"
+    "metadata" = {
+      "name"      = "${local.project}-ingress-external-dns"
+      "namespace" = data.kubernetes_namespace_v1.main.metadata[0].name
+      "labels"    = local.tags
+    }
+    "spec" = {
+      "rules" = [
+        for host in local.ingress.hosts :
+        {
+          "host" = host
+        }
+      ]
+    }
+  }
+}
