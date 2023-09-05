5208904d779b5568722d2bf4adfd7f8847072557
Emil Dragańczuk <emil.draganczuk@leancode.pl>
2023-09-05 09:57:24 +0200

    Bring back the repository fix

--- backend/src/Core/ExampleApp.Core.Services/DataAccess/Repositories/ProjectsRepository.cs
+++ backend/src/Core/ExampleApp.Core.Services/DataAccess/Repositories/ProjectsRepository.cs
@@ -11,6 +11,32 @@ public class ProjectsRepository : EFRepository<Project, ProjectId, CoreDbContext
     public ProjectsRepository(CoreDbContext dbContext)
         : base(dbContext) { }
 
+    public override void Add(Project entity)
+    {
+        ((IOptimisticConcurrency)entity).DateModified = Time.NowWithOffset.UtcDateTime;
+        DbSet.Add(entity);
+    }
+
+    public override void Delete(Project entity)
+    {
+        ((IOptimisticConcurrency)entity).DateModified = Time.NowWithOffset.UtcDateTime;
+        DbSet.Remove(entity);
+    }
+
+    public override void DeleteRange(IEnumerable<Project> entities)
+    {
+        foreach (var item in entities)
+        {
+            ((IOptimisticConcurrency)item).DateModified = Time.NowWithOffset.UtcDateTime;
+        }
+        DbSet.RemoveRange(entities);
+    }
+
+    public override void Update(Project entity)
+    {
+        ((IOptimisticConcurrency)entity).DateModified = Time.NowWithOffset.UtcDateTime;
+    }
+
     public override Task<Project?> FindAsync(ProjectId id, CancellationToken cancellationToken = default)
     {
         return DbSet.AsTracking().FirstOrDefaultAsync(p => p.Id == id, cancellationToken)!;
