a8e39a57745b03d6679a120c83b404831573508e
Łukasz Garstecki <lukasz.garstecki@leancode.pl>
2023-09-25 12:16:45 +0200

    Fix storage connection string in integration tests

--- backend/dev/config/integration_tests.sh
+++ backend/dev/config/integration_tests.sh
@@ -5,7 +5,7 @@ export Kratos__AdminEndpoint='http://localhost'
 export Kratos__WebhookApiKey='Passw12#'
 
 export PostgreSQL__ConnectionStringBase='Host=postgresql-svc.shared.svc.cluster.local;Username=postgres;Password=Passw12#'
-export BlobStorage__ConnectionString='DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://storage.local.lncd.pl:10000/devstoreaccount1;'
+export BlobStorage__ConnectionString='DefaultEndpointsProtocol=http;AccountName=blobstorage;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://blobstorage.blob.svc.cluster.local/;'
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
