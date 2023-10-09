029cfa58c868cc0d0963db018a82a6c3aeda8501
Łukasz Garstecki <lukasz.garstecki@leancode.pl>
2023-10-09 17:30:09 +0200

    Add missing migration

--- backend/src/Apps/ExampleApp.Migrations/Migrations/20231009152950_RemoveDomainTypesFromDb.Designer.cs
+++ backend/src/Apps/ExampleApp.Migrations/Migrations/20231009152950_RemoveDomainTypesFromDb.Designer.cs
@@ -4,6 +4,7 @@ using System.Text.Json;
 using ExampleApp.Core.Services.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ExampleApp.Migrations.Migrations
 {
     [DbContext(typeof(CoreDbContext))]
-    partial class CoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231009152950_RemoveDomainTypesFromDb")]
+    partial class RemoveDomainTypesFromDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
