a3b9215af06131bd86e499128e8674473a0a53cf
Emil Dragańczuk <emil.draganczuk@leancode.pl>
2023-10-23 13:13:30 +0200

    Use default RabbitMQ image on the dev env

--- dev-cluster/rabbitmq.tf
+++ dev-cluster/rabbitmq.tf
@@ -1,31 +1,3 @@
-locals {
-  rabbit_image_name = "${local.registry_address}/rabbit"
-  rabbit_full_name  = "${local.rabbit_image_name}:latest"
-}
-
-resource "docker_image" "rabbit" {
-  count = var.rabbit ? 1 : 0
-
-  name = local.rabbit_full_name
-
-  build {
-    context    = "./apps"
-    dockerfile = "Dockerfile.rabbitmq"
-    build_arg  = {
-      dockerfile_trigger = filemd5("./apps/Dockerfile.rabbitmq")
-    }
-  }
-
-  # Docker provider cannot push to insecure registries
-  provisioner "local-exec" {
-    command = "docker push ${local.rabbit_full_name}"
-  }
-
-  depends_on = [
-    null_resource.cluster
-  ]
-}
-
 resource "helm_release" "rabbit" {
   count = var.rabbit ? 1 : 0
 
@@ -34,21 +6,6 @@ resource "helm_release" "rabbit" {
 
   chart      = "rabbitmq"
   repository = "https://charts.bitnami.com/bitnami"
-
-  set {
-    name  = "image.registry"
-    value = local.registry_address
-  }
-
-  set {
-    name  = "image.repository"
-    value = "rabbit"
-  }
-
-  set {
-    name  = "image.tag"
-    value = "latest"
-  }
 
   set {
     name  = "auth.username"
@@ -69,10 +26,6 @@ resource "helm_release" "rabbit" {
     name  = "auth.erlangCookie"
     value = "AZ6s6VSejs7Fgicj1G8mORMcnVwscVnN"
   }
-
-  depends_on = [
-    docker_image.rabbit[0]
-  ]
 }
 
 resource "kubernetes_manifest" "rabbit_ingress" {
