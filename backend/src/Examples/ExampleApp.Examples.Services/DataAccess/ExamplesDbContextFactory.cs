97f9d389675b95b6ef6decf104d4d0bc0377bb38
Wojciech Klusek <wojciech.klusek@leancode.pl>
2024-01-10 15:26:17 +0100

    Use dbcontext factory for migration bundle

---
+++ backend/src/Examples/ExampleApp.Examples.Services/DataAccess/ExamplesDbContextFactory.cs
@@ -0,0 +1,39 @@
+using LeanCode.AzureIdentity;
+using LeanCode.Npgsql.ActiveDirectory;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Npgsql;
+
+namespace ExampleApp.Examples.Services.DataAccess;
+
+/// <remarks> Used for `dotnet ef migrations bundle`. </remarks>
+public class ExamplesDbContextFactory : IDesignTimeDbContextFactory<ExamplesDbContext>
+{
+    private const string ConnectionStringKey = "PostgreSQL__ConnectionString";
+    private static readonly NpgsqlDataSource DataSource = CreateDataSource();
+
+    private static NpgsqlDataSource CreateDataSource()
+    {
+        var connectionString =
+            Environment.GetEnvironmentVariable(ConnectionStringKey)
+            ?? throw new InvalidOperationException("Failed to find connection string.");
+
+        var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionString);
+
+        if (dataSourceBuilder.ConnectionStringBuilder.Password is null)
+        {
+            dataSourceBuilder.UseAzureActiveDirectoryAuthentication(DefaultLeanCodeCredential.CreateFromEnvironment());
+        }
+
+        return dataSourceBuilder.Build();
+    }
+
+    public ExamplesDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ExamplesDbContext>();
+
+        optionsBuilder.UseNpgsql(DataSource.CreateConnection());
+
+        return new ExamplesDbContext(optionsBuilder.Options);
+    }
+}
