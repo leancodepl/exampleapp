c8032f1ebdedcc9cf28a59b29a7cbaa2b1b8ed51
Emil Dragańczuk <emil.draganczuk@leancode.pl>
2024-01-11 11:50:33 +0100

    Generate migration

--- backend/src/Examples/ExampleApp.Examples.Migrations/Migrations/20240109131424_AddProjectLeaders.Designer.cs
+++ backend/src/Examples/ExampleApp.Examples.Migrations/Migrations/20240109131424_AddProjectLeaders.Designer.cs
@@ -5,6 +5,7 @@ using System.Text.Json;
 using ExampleApp.Examples.Services.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -13,9 +14,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ExampleApp.Examples.Migrations.Migrations
 {
     [DbContext(typeof(ExamplesDbContext))]
-    partial class ExamplesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240109131424_AddProjectLeaders")]
+    partial class AddProjectLeaders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/src/Examples/ExampleApp.Examples.Migrations/Migrations/20240109131424_AddProjectLeaders.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExampleApp.Examples.Migrations.Migrations;
+
+/// <inheritdoc />
+public partial class AddProjectLeaders : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<string>(name: "ProjectLeaderId", table: "Projects", type: "citext", nullable: true);
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(name: "ProjectLeaderId", table: "Projects");
+    }
+}
