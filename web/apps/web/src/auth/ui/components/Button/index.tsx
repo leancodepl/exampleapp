41e5290b5d2f17c5a47167952c683301da1ff494
Aleksander Szczepura <al.szczepura@gmail.com>
2024-08-28 13:09:06 +0200

    Fix passkey and passkey button variant

--- web/apps/web/src/auth/ui/components/Button/index.tsx
+++ web/apps/web/src/auth/ui/components/Button/index.tsx
@@ -1,9 +1,9 @@
 import { Button } from "antd"
 import { ButtonComponentProps } from "@leancodepl/kratos"
 
-export function ButtonComponent({ header, fullWidth: _fullWidth, type, ...props }: ButtonComponentProps) {
+export function ButtonComponent({ header, fullWidth: _fullWidth, node: _node, type, ...props }: ButtonComponentProps) {
     return (
-        <Button block {...props} htmlType={type} type={type === "submit" ? "primary" : "default"}>
+        <Button block {...props} htmlType={type} type="primary">
             {header}
         </Button>
     )
