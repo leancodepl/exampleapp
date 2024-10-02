c7f0204c1085b95e40a5f63d15805a253007b620
Jakub Fijałkowski <kuba@codinginfinity.me>
2024-10-02 16:14:40 +0200

    Add admin commands

---
+++ backend/src/Examples/ExampleApp.Examples.Contracts/Booking/Admin/AddTimeslot.cs
@@ -0,0 +1,26 @@
+using LeanCode.Contracts;
+using LeanCode.Contracts.Security;
+
+namespace ExampleApp.Examples.Contracts.Booking.Admin;
+
+[AuthorizeWhenHasAnyOf(Auth.Roles.Admin)]
+public class AddTimeslot : ICommand
+{
+    public string ServiceProviderId { get; set; }
+    public DateOnly Date { get; set; }
+    public TimeOnly StartTime { get; set; }
+    public TimeOnly EndTime { get; set; }
+    public MoneyDTO Price { get; set; }
+
+    public static class ErrorCodes
+    {
+        public const int ServiceProviderIdIsNullOrEmpty = 1;
+        public const int DateIsInvalid = 2;
+        public const int StartTimeIsInvalid = 3;
+        public const int EndTimeIsInvalid = 4;
+        public const int EndTimeMustBeAfterStartTime = 5;
+        public const int PriceIsNull = 6;
+        public const int PriceCurrencyIsInvalid = 7;
+        public const int TimeslotOverlapsWithExisting = 8;
+    }
+}
