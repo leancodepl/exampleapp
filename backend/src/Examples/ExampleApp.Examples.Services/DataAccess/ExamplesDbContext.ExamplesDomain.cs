c322863b65513615241c1d461c91c2f8caec1d9a
Jakub Fijałkowski <kuba@codinginfinity.me>
2024-10-08 22:40:22 +0200

    Wrap `RowVersion` cfg into an extension method and make `ConfigureId` an extension method

--- backend/src/Examples/ExampleApp.Examples.Services/DataAccess/ExamplesDbContext.ExamplesDomain.cs
+++ backend/src/Examples/ExampleApp.Examples.Services/DataAccess/ExamplesDbContext.ExamplesDomain.cs
@@ -24,12 +24,12 @@ public partial class ExamplesDbContext : IAppRatingStore<Guid>
 
     private void ConfigureExampleAppConventions(ModelConfigurationBuilder configurationBuilder)
     {
-        ConfigureId<EmployeeId>(configurationBuilder);
-        ConfigureId<ProjectId>(configurationBuilder);
-        ConfigureId<AssignmentId>(configurationBuilder);
-        ConfigureId<ServiceProviderId>(configurationBuilder);
-        ConfigureId<CalendarDayId>(configurationBuilder);
-        ConfigureId<TimeslotId>(configurationBuilder);
+        configurationBuilder.ConfigureId<EmployeeId>();
+        configurationBuilder.ConfigureId<ProjectId>();
+        configurationBuilder.ConfigureId<AssignmentId>();
+        configurationBuilder.ConfigureId<ServiceProviderId>();
+        configurationBuilder.ConfigureId<CalendarDayId>();
+        configurationBuilder.ConfigureId<TimeslotId>();
     }
 
     private void OnExampleModelCreating(ModelBuilder modelBuilder)
@@ -42,7 +42,7 @@ public partial class ExamplesDbContext : IAppRatingStore<Guid>
             e.HasKey(t => t.Id);
 
             e.IsOptimisticConcurrent(addRowVersion: false);
-            e.Property<uint>("xmin").IsRowVersion();
+            e.HasRowVersion();
         });
 
         modelBuilder.Entity<Project>(e =>
@@ -60,7 +60,7 @@ public partial class ExamplesDbContext : IAppRatingStore<Guid>
             );
 
             e.IsOptimisticConcurrent(addRowVersion: false);
-            e.Property<uint>("xmin").IsRowVersion();
+            e.HasRowVersion();
         });
 
         modelBuilder.Entity<ServiceProvider>(e =>
@@ -70,7 +70,7 @@ public partial class ExamplesDbContext : IAppRatingStore<Guid>
             e.ComplexProperty(t => t.Location);
 
             e.IsOptimisticConcurrent(addRowVersion: false);
-            e.Property<uint>("xmin").IsRowVersion();
+            e.HasRowVersion();
         });
 
         modelBuilder.Entity<CalendarDay>(e =>
@@ -82,7 +82,7 @@ public partial class ExamplesDbContext : IAppRatingStore<Guid>
             e.Navigation(t => t.Timeslots).AutoInclude();
 
             e.IsOptimisticConcurrent(addRowVersion: false);
-            e.Property<uint>("xmin").IsRowVersion();
+            e.HasRowVersion();
         });
 
         modelBuilder.Entity<Timeslot>(e =>
@@ -101,13 +101,24 @@ public partial class ExamplesDbContext : IAppRatingStore<Guid>
             });
         });
     }
+}
 
-    private static PropertiesConfigurationBuilder<TId> ConfigureId<TId>(ModelConfigurationBuilder configurationBuilder)
+file static class ModelConfigurationBuilderExtensions
+{
+    public static PropertiesConfigurationBuilder<TId> ConfigureId<TId>(
+        this ModelConfigurationBuilder configurationBuilder
+    )
         where TId : struct, IPrefixedTypedId<TId>
     {
         return configurationBuilder
             .Properties<TId>()
             .HaveColumnType("citext")
             .HaveConversion<PrefixedTypedIdConverter<TId>, PrefixedTypedIdComparer<TId>>();
+    }
+
+    public static void HasRowVersion<TEntity>(this EntityTypeBuilder<TEntity> e)
+        where TEntity : class
+    {
+        e.Property<uint>("xmin").IsRowVersion();
     }
 }
