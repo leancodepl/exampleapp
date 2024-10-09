d3313da8e1f963976b39055dfdaf01fa471015db
Jakub Fijałkowski <kuba@codinginfinity.me>
2024-10-09 09:07:16 +0200

    Add tests for ToDTO/ToDomain extension methods for Money and Location

--- backend/src/Examples/ExampleApp.Examples.Domain/Booking/Location.cs
+++ backend/src/Examples/ExampleApp.Examples.Domain/Booking/Location.cs
@@ -2,4 +2,4 @@ using LeanCode.DomainModels.Model;
 
 namespace ExampleApp.Examples.Domain.Booking;
 
-public record Location(double Longitude, double Latitude) : ValueObject;
+public record Location(double Latitude, double Longitude) : ValueObject;
--- backend/src/Examples/ExampleApp.Examples.Services/CQRS/Booking/LocationDTOExtensions.cs
+++ backend/src/Examples/ExampleApp.Examples.Services/CQRS/Booking/LocationDTOExtensions.cs
@@ -5,7 +5,7 @@ namespace ExampleApp.Examples.Services.CQRS.Booking;
 
 public static class LocationDTOExtensions
 {
-    public static LocationDTO ToDTO(this Location l) => new LocationDTO(l.Latitude, l.Longitude);
+    public static LocationDTO ToDTO(this Location l) => new(l.Latitude, l.Longitude);
 
-    public static Location ToDomain(this LocationDTO l) => new Location(l.Latitude, l.Longitude);
+    public static Location ToDomain(this LocationDTO l) => new(l.Latitude, l.Longitude);
 }
