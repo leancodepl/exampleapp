dbc9c2fe5e004a1cbc6b7f750178e0fe3cbe621d
Jakub Fijałkowski <kuba@codinginfinity.me>
2024-12-04 13:34:07 +0100

    Update CSharpier

--- backend/tests/Examples/ExampleApp.Examples.Tests/EventSerializationTests.Data.cs
+++ backend/tests/Examples/ExampleApp.Examples.Tests/EventSerializationTests.Data.cs
@@ -23,45 +23,44 @@ public partial class EventSerializationTests
 #endif
     private static readonly Guid Guid = Guid.NewGuid();
     private static readonly DateTime DateTime = DateTime.UtcNow;
-    private static readonly Identity KratosIdentity =
-        new()
-        {
-            State = IdentityState.Active,
-            CreatedAt = DateTime,
-            Id = Guid,
-            RecoveryAddresses =
-            [
-                new()
-                {
-                    CreatedAt = DateTime,
-                    Id = Guid,
-                    UpdatedAt = DateTime,
-                    Value = "test@leancode.pl",
-                    Via = AddressType.Email,
-                },
-            ],
-            SchemaId = "user",
-            SchemaUrl = new("https://auth.exampleapp.test.lncd.pl/schemas/dXNlcg"),
-            StateChangedAt = DateTime,
-            UpdatedAt = DateTime,
-            VerifiableAddresses =
-            [
-                new()
-                {
-                    CreatedAt = DateTime,
-                    Id = Guid,
-                    Status = AddressStatus.Completed,
-                    UpdatedAt = DateTime,
-                    Value = "test@leancode.pl",
-                    Verified = true,
-                    VerifiedAt = DateTime,
-                    Via = AddressType.Email,
-                },
-            ],
-            MetadataAdmin = JsonSerializer.Deserialize<JsonElement>(@"{""foo"":""bar""}"),
-            MetadataPublic = JsonSerializer.Deserialize<JsonElement>(@"{""roles"":[""user""]}"),
-            Traits = JsonSerializer.Deserialize<JsonElement>(@"{""email"":""test@leancode.pl""}"),
-        };
+    private static readonly Identity KratosIdentity = new()
+    {
+        State = IdentityState.Active,
+        CreatedAt = DateTime,
+        Id = Guid,
+        RecoveryAddresses =
+        [
+            new()
+            {
+                CreatedAt = DateTime,
+                Id = Guid,
+                UpdatedAt = DateTime,
+                Value = "test@leancode.pl",
+                Via = AddressType.Email,
+            },
+        ],
+        SchemaId = "user",
+        SchemaUrl = new("https://auth.exampleapp.test.lncd.pl/schemas/dXNlcg"),
+        StateChangedAt = DateTime,
+        UpdatedAt = DateTime,
+        VerifiableAddresses =
+        [
+            new()
+            {
+                CreatedAt = DateTime,
+                Id = Guid,
+                Status = AddressStatus.Completed,
+                UpdatedAt = DateTime,
+                Value = "test@leancode.pl",
+                Verified = true,
+                VerifiedAt = DateTime,
+                Via = AddressType.Email,
+            },
+        ],
+        MetadataAdmin = JsonSerializer.Deserialize<JsonElement>(@"{""foo"":""bar""}"),
+        MetadataPublic = JsonSerializer.Deserialize<JsonElement>(@"{""roles"":[""user""]}"),
+        Traits = JsonSerializer.Deserialize<JsonElement>(@"{""email"":""test@leancode.pl""}"),
+    };
 
     private static readonly ImmutableArray<object> Events =
     [
