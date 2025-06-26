e5a0f4cb3774a010a655b128568134e597f104ae
Krzysztof Bogacki <krzysztof.bogacki@leancode.pl>
2025-06-26 17:00:14 +0200

    Expand capabilities of SearchIdentities query

--- backend/src/Examples/ExampleApp.Examples.Contracts/Identities/SearchIdentities.cs
+++ backend/src/Examples/ExampleApp.Examples.Contracts/Identities/SearchIdentities.cs
@@ -4,12 +4,20 @@ using LeanCode.Contracts.Security;
 namespace ExampleApp.Examples.Contracts.Identities;
 
 [AuthorizeWhenHasAnyOf(Auth.Roles.Admin)]
-public class SearchIdentities : PaginatedQuery<KratosIdentityDTO>
+public class SearchIdentities : SortedQuery<KratosIdentityDTO, KratosIdentitySortKeyDTO>
 {
     public string? SchemaId { get; set; }
     public string? EmailPattern { get; set; }
     public string? GivenNamePattern { get; set; }
     public string? FamilyNamePattern { get; set; }
+}
+
+public enum KratosIdentitySortKeyDTO
+{
+    CreatedAt = 0,
+    Email = 1,
+    GivenName = 2,
+    FamilyName = 3,
 }
 
 public class KratosIdentityDTO
@@ -19,7 +27,15 @@ public class KratosIdentityDTO
     public DateTimeOffset UpdatedAt { get; set; }
     public string SchemaId { get; set; }
     public string Email { get; set; }
+    public string GivenName { get; set; }
+    public string? FamilyName { get; set; }
 
     [ExcludeFromContractsGeneration]
     public object Traits { get; set; }
+
+    [ExcludeFromContractsGeneration]
+    public object? MetadataPublic { get; set; }
+
+    [ExcludeFromContractsGeneration]
+    public object? MetadataAdmin { get; set; }
 }
